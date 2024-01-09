c9381ac3c2c1abcae8cb321dd318afbe2792fecd
Elena Miller <em_dev_9836@outlook.com>
2024-01-09 17:55:15 -0600

    moved files

---
+++ html/js/gui.js
@@ -0,0 +1,30 @@
+/**
+ * @file gui
+ * @fileoverview contains gui functions
+ *
+ * 
+ */
+
+/**
+ * 
+ *
+ * @param {string} iframe
+ */
+const clear_frame = function (iframe = "") {
+    $id(iframe).innerHTML = "";
+}
+const add = function (iframe = "", content) {
+
+}
+
+const gui = {
+    clear_iframe: function (id = "") {
+        if (id === "") {
+            console.error("Missing Iframe id.");
+        } else {
+            $id(id).innerHTML = "";
+        }
+    },
+    add: function ()
+
+}
