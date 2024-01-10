00a6c65830eb672ffdd5f87df8e6c98c244873d7
Elena Miller <em_dev_9836@outlook.com>
2024-01-09 21:31:18 -0600

    update files and added new ones

---
+++ html/js/bib-script.js
@@ -0,0 +1,18 @@
+/**
+    * @description      : 
+    * @author           : x
+    * @group            : 
+    * @created          : 09/01/2024 - 20:32:36
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 09/01/2024
+    * - Author          : x
+    * - Modification    : 
+**/
+
+
+
+$(document).ready((evt) => {
+    add_books();
+});
