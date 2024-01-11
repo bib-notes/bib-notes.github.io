3aefcd19922b25d4f29194ac94324af8a6a4410e
Elena Miller <em_dev_9836@outlook.com>
2024-01-10 20:17:42 -0600

    Update front-end.js
    script created section buttons for section list.

--- html/js/front-end.js
+++ html/js/front-end.js
@@ -3,16 +3,39 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 function load_secList() {
     let list = $id("sec-list"), sects = document.getElementsByClassName("main-section");
     var i = 0;
-    for (i = 0; i < sects; i++) {
+    var btn = null;
+    var sect = null, link = "";//for name
+    var text = "";
+    console.log(sects);
+    for (i = 0; i < sects.length; i++) {
+        btn = document.createElement("button");
+        sect = sects[i];
+        console.log(sect);
+        link = sect.id;
+        text = "_" + link;
+        // btn.class = "sec-btn";
+        btn.setAttribute("class", "sec-btn");
+        btn.setAttribute("data-link", link);
 
+        if (sect.getAttribute("data-toggle") === "on") {
+            btn.setAttribute("data-active","");
+        }
+      
+
+        btn.innerHTML = text;
+        list.appendChild(btn);
+        console.log(i);
     }
 
 }
-function change_color() {
-
+function set_theme(color) {
+    if (color === "blue" || color === "green" || color === "red") {
+        $id("screen").setAttribute("data-color", color);
+    }
 }
 function set_title() {
     let title = document.getElementById("title");//$id("title");
@@ -23,11 +46,14 @@ $(document).ready((evt) => {
     // title
     set_title();
     //Section List
-
+    load_secList();
     // color
     let colors = document.getElementsByClassName("change-color");
     for (var i = 0; i < colors.length; i++) {
-        console.log(colors[i]);
+        // console.log(colors[i]);
+        let color_btn = colors[i];
+        let val = color_btn.getAttribute("value");
+        color_btn.setAttribute("onclick","set_theme('"+val+"');");
     }
 });
 
